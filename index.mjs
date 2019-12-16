bb4e38e36503fda832382459eab8ce1a103dcd8e
pierre <nanashi4781@gmail.com>
2019-12-16 16:50:08 +0100

    create-api

--- index.mjs
+++ index.mjs
@@ -3,6 +3,10 @@ import cors from 'cors'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 mongoose.connect('mongodb://localhost/api-test', {
     useCreateIndex: true,
@@ -33,21 +37,84 @@ app.use(cors({
 
 app.use(bodyParser.json())
 
+function verifyToken (req, res, next) {
+    let token = req.headers.authorization
+    if (typeof token === 'string' &&
+    token.startsWith('Bearer ')){
+        token = token.substring(7)
+        try{
+            jwt.verify(token, process.env.SECRET)
+            return next()
+        }catch (e) {
+            resp.status(401)
+            res.json({
+                error: "Invalid access token"
+            })
+        }
+    }else{
+        res.status(401)
+        res.json({
+            error: "Acces token is required"
+        })
+    }
+}
+
+app.get('/me', verifyToken, (req, res)=>{
+    res.send('hey oh')
+})
+
+
 app.get('*', (req, res)=>{
     res.status(404)
     res.send("la requête est introuvable")
 })
 
-app.post('/user',(req, res)=>{
-    const email =req.body.email
-    const password =req.body.password
-    const name =req.body.name
+app.post('/user',async(req, res)=>{
+    const email = req.body.email
+    const password = req.body.password
+    const name = req.body.name
     //controle des ID//
     const hash = bcryptjs.hashSync(password, 8)
-
-    console.log(hash)
+    const newUser = new user({
+        email: email,
+        password: hash,
+        name: name,
+    })
+    try {
+        const data = (await newUser.save()).toObject()
+        delete data.password
+        res.json (data)
+    }catch (e){
+        res.status(401)
+        res.json({
+            error: e.errmsg
+        })
+    }
 })
 
+app.post('/login',async(req, res)=>{
+    const email = req.body.email
+    const password = req.body.password
+
+    const data = await user.findOne({
+        email
+    })
+    if (bcryptjs.compareSync(password, data.password)){
+        const token = jwt.sign({
+            id: data._id,
+            name: data.name,
+            email: data.email,
+        },process.env.SECRET, {
+            expiresIn: 86400 //60*60*24=24 heures//
+        })
+        res.json({
+            token: token
+        })
+    }else{
+
+    }
+    console.log(data)
+})
 
 app.listen(3000, ()=>{
     console.log('http://localhost:3000')
